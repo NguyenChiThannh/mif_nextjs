e89761c253aa8105714d1e90684c3dddd41b20e0
Nguyễn Chí Thanh <92113679+NguyenChiThannh@users.noreply.github.com>
2024-12-12 01:38:13 +0700

    fix

--- src/app/[locale]/(root)/live/page.jsx
+++ src/app/[locale]/(root)/live/page.jsx
@@ -1,8 +1,11 @@
+'use client'
+import useUserId from '@/hooks/useUserId'
 import { userApi } from '@/services/userApi'
 import React from 'react'
 
 export default function LiveStreamPage() {
-    const { data, isLoading } = userApi.query.useGetUserInfoById(id)
+    const userId = useUserId()
+    const { data, isLoading } = userApi.query.useGetUserInfoById(userId)
     console.log('🚀 ~ LiveStreamPage ~ data:', data)
     console.log('🚀 ~ LiveStreamPage ~ isLoading:', isLoading)
     return (
