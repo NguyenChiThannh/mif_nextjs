276ab7157360f0006fab14f7b23d9f52856239f6
Nguyễn Chí Thanh <92113679+NguyenChiThannh@users.noreply.github.com>
2025-05-06 16:24:57 +0700

    Change bg

--- src/app/[locale]/(auth)/bg-movie/background.jsx
+++ src/app/[locale]/(auth)/bg-movie/background.jsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 import "./style.css"
 
 const images = [
-    'https://thanhnien.mediacdn.vn/Uploaded/phongdt/2022_08_04/spider-man-2363.jpg',
     'https://facts.net/wp-content/uploads/2023/06/37-facts-about-the-movie-titanic-1687656865.jpg',
+    'https://cdn.galaxycine.vn/media/2024/6/28/twisters-750_1719558114559.jpg',
     'https://live.staticflickr.com/4005/4686746190_31c292dc8c_b.jpg',
     'https://cdna.artstation.com/p/assets/images/images/017/022/542/large/amirhosein-naseri-desktop-screenshot-2019-04-03-18-17-47-11.jpg?1554338571',
     'https://townsquare.media/site/442/files/2013/05/man-of-steel-poster-banner.jpg',
     'https://cdnmedia.baotintuc.vn/2017/04/13/13/44/fastfurious.jpg',
     'https://images.squarespace-cdn.com/content/v1/5fbc4a62c2150e62cfcb09aa/1657909007498-F339J4NXVFU4CAIJRDIE/the%2Bgodfather%2Bthumbnail%2Breal.png',
     'https://i.ytimg.com/vi/UmDVjN7EetM/maxresdefault.jpg',
-    'https://cdn.galaxycine.vn/media/2024/6/28/twisters-750_1719558114559.jpg',
     'https://i.ytimg.com/vi/S1dnnQsY0QU/maxresdefault.jpg',
     'https://i.insider.com/525041676bb3f72b5fb018d9?width=1200',
+    'https://thanhnien.mediacdn.vn/Uploaded/phongdt/2022_08_04/spider-man-2363.jpg',
     'https://images2.thanhnien.vn/528068263637045248/2023/12/16/the-last-of-us-17027156340062009094036.jpg',
     'https://i.pinimg.com/736x/0a/7b/6d/0a7b6dfba555c5f7d8e752e22d099d6f.jpg',
     'https://i.pinimg.com/736x/63/7b/72/637b7255eb38b716439c62ba7cce5368.jpg',
