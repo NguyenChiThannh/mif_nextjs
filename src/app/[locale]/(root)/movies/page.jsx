e1680a562a666a6da88e3a15c5db56402c8aabe6
Nguyễn Chí Thanh <92113679+NguyenChiThannh@users.noreply.github.com>
2025-05-19 17:02:20 +0700

    Feat: Click Chat in group join group chat

--- src/app/[locale]/(root)/movies/page.jsx
+++ src/app/[locale]/(root)/movies/page.jsx
@@ -55,16 +55,16 @@ export default function MoviePage() {
     return (
         <div className='grid grid-cols-3 gap-10'>
             <div className='grid col-span-2'>
-                <div className='flex justify-around'>
+                <div className='flex'>
                     <div className='flex items-center'>
                         <div className='w-2 bg-primary h-8 rounded'></div>
                         <h1 className='text-xl md:text-xl lg:text-2xl font-bold pl-4'>{t("title")}</h1>
                     </div>
-                    <div className='ml-auto mt-4 gap-2'>
+                    {/* <div className='ml-auto mt-4 gap-2'>
                         <div className='flex gap-2 items-center'>
                             <ComboboxMovieCategory />
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className='grid mt-4 gap-2'>
                     {isLoading && (
